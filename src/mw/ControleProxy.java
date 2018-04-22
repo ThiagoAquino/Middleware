6e1ae5732892d6eb679e73436e5a65f3ff4d2f61
Allan <ala6@cin.ufpe.br>
2018-04-22 17:11:19 -0300

    pequena continuação no Controle

--- src/mw/ControleProxy.java
+++ src/mw/ControleProxy.java
@@ -1,10 +1,53 @@
 package mw;
 
-public class ControleProxy extends ClientProxy implements IControle{
+import Message.Message;
 
-	public ControleProxy() {
-		super();
-		// TODO Auto-generated constructor stub
+public class ControleProxy extends ClientProxy implements IControle {
+	
+	private String ip;
+	private int port;
+	
+	public ControleProxy(String ip, int port) {
+		this.ip = ip;
+		this.port = port;
 	}
+
+	@Override
+	public int up(int vol) {
+		
+        Resquestor req = new Resquestor();
+        Message msg = new Message();
+
+        //Executar um serviço
+        //Passar o vol pela msg + tipo do serviço que no caso é aumentar
+        //E receber de volta o volume novo para retornar
+        //inv: this.ip, this.port + msg
+        
+        Message rMsg = req.invoke(inv);           
+
+		int nVol = rMsg.
+		
+		return nVol;
+	}
+
+	@Override
+	public int down(int vol) {
+		return vol--;
+	}
+
+	@Override
+	public int nextChannel(int channel) {
+		return channel++;
+	}
+
+	@Override
+	public int previousChannel(int channel) {
+		return channel--;
+	}
+
+//	@Override
+//	public int changeChannel(int channel) {
+//		return null;
+//	}
 
 }
--- src/mw/IControle.java
+++ src/mw/IControle.java
@@ -1,5 +1,10 @@
 package mw;
 
 public interface IControle {
-
+	
+	public int up(int vol);
+	public int down(int vol);
+	public int nextChannel(int channel);
+	public int previousChannel(int channel);
+//	public int changeChannel(int channel);
 }
