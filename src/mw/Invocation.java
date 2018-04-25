f7c0ec66a98e71f5a0f7d569abb2827218094f91
Allan <ala6@cin.ufpe.br>
2018-04-24 23:15:45 -0300

    invocation

---
+++ src/mw/Invocation.java
@@ -0,0 +1,28 @@
+package mw;
+
+public class Invocation {
+	
+	private String id;
+	private int port;
+	
+    public Invocation() {
+
+    }
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+}
